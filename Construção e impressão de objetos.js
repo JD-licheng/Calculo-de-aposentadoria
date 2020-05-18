6f44a0ff16c0710664ab8ec9998acb44633382f6
JD-licheng <65181655+JD-licheng@users.noreply.github.com>
2020-05-17 22:06:16 -0300

    Construção e impressão de objetos
    Desafio: Imprimir em tela utilizando console.log o nome da empresa e seu endereço no seguinte formato:
    
    A empresa Rocketseat está localizada em Rua Guilherme Gembala, 260

---
+++ Construção e impressão de objetos.js
@@ -0,0 +1,11 @@
+const empresa = {
+    nome : "Rocketseat",
+    cor : "Roxo",
+    Foco : "Programação",
+    Endereço: {
+        Rua : "Rua Guilherme Gembala",
+        Número : 260
+    }
+}
+
+console.log(`A empresa ${empresa.nome} está localizada em ${empresa.Endereço.Rua}, ${empresa.Endereço.Número}`)
